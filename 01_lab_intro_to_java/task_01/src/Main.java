c23b3adc0e4f5cca67b77ae45bc923e86e197491
BilyanaH <bilqna2015@gmail.com>
2025-10-24 13:45:20 +0300

    Add minDifference function and update project files
    Introduced the minDifference function to distribute tasks with minimal difference between two groups.

--- 01_lab_intro_to_java/task_01/src/Main.java
+++ 01_lab_intro_to_java/task_01/src/Main.java
@@ -15,12 +15,43 @@ public static String longestUniqueSubstring(String s){
     }
     return longest;
 }
+//02-task-distributor
+public static int minDifference(int[] tasks){
+    int n = tasks.length;
+    int sum=0;
+    for (int i=0; i < n; i++) {
+        sum+=tasks[i];
+    }
+    boolean[] sums_to_n = new boolean[sum+1];
+    sums_to_n[0]=true;
 
+    for (int i=0; i<n; i++){
+            for (int j=sum; j>=tasks[i]; j--) {
+                if (sums_to_n[j - tasks[i]]) {
+                    sums_to_n[j]=true;
+                }
+            }
+    }
+    for (int i=sum/2; i>=0; i--){
+        if(sums_to_n[i]){
+            return sum-i*2;
+        }
+    }
+    return -1;
+}
 void main() {
-    System.out.println(longestUniqueSubstring("abcabcbb"));
-    System.out.println(longestUniqueSubstring("bbbbb"));
-    System.out.println(longestUniqueSubstring("pwwkew"));
-    System.out.println(longestUniqueSubstring("abcdefg"));
-    System.out.println(longestUniqueSubstring("x"));
-    System.out.println(longestUniqueSubstring(""));
+    System.out.println(longestUniqueSubstring("abcabcbb")); //"abc"
+    System.out.println(longestUniqueSubstring("bbbbb")); //"b"
+    System.out.println(longestUniqueSubstring("pwwkew")); //"wke
+    System.out.println(longestUniqueSubstring("abcdefg")); //"abcdefg"
+    System.out.println(longestUniqueSubstring("x")); //"x"
+    System.out.println(longestUniqueSubstring("")); //""
+
+    System.out.println(minDifference(new int[]{1,2,3,4,5})); //1
+    System.out.println(minDifference(new int[]{10,20,15,5})); //0
+    System.out.println(minDifference(new int[]{7,3,2,1,5,4})); //0
+    System.out.println(minDifference(new int[]{9,1,1,1})); //6
+    System.out.println(minDifference(new int[]{})); //0
+    System.out.println(minDifference(new int[]{120})); //120
+    System.out.println(minDifference(new int[]{30, 30})); //0
 }
